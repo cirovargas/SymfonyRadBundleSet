c0e4390e58b9dc69abb9a4683dd79c1e151b7332
Ciro Vargas <cirobsb_br@yahoo.com.br>
2015-09-23 00:55:18 -0300

    Update script-rad.sh

--- script-rad.sh
+++ script-rad.sh
@@ -10,14 +10,14 @@ curl -sS https://getcomposer.org/installer | php
 php composer.phar install
 php app/console doctrine:mapping:import AppBundle annotation
 sleep 5
-#for f in  src/Project/AdminBundle/Entity/* ; do folder=(${f//\// }) ; entity=${folder[${#folder[@]}-1]//\.php/ }; php app/console doctrine:generate:entities ProjectAdminBundle:${entity} ; done
 php app/console doctrine:generate:entities AppBundle
 sleep 5
-
 rm src/AppBundle/Entity/*.php~
+sleep 5
 php app/console doctrine:schema:update --force --complete
+sleep 5
 php app/console fos:user:create
-
+sleep 5
 for f in  src/Project/AdminBundle/Entity/* ; do folder=(${f//\// }) ; entity=${folder[${#folder[@]}-1]//\.php/ }; php app/console jordillonch:generate:crud --entity=AppBundle:${entity} --route-prefix=/${entity} --with-write --format=yml --overwrite --no-interaction   ; done
 php app/console cache:clear
 
